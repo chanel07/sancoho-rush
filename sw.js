d7e74d78c37912265e85c92e1edcf0efbdef6d57
chanel07 <82330098+chanel07@users.noreply.github.com>
2026-08-26 23:36:12 -0500

    Add service worker for asset caching
    Implement service worker for caching assets and handling fetch requests.

---
+++ sw.js
@@ -0,0 +1,34 @@
+var CACHE='sancocho-rush-v1';
+var ASSETS=['/','/index.html','/manifest.json','/icons/icon-192.png','/icons/icon-512.png'];
+self.addEventListener('install',function(e){
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(function(c){
+    return Promise.all(ASSETS.map(function(u){
+      return c.add(u).catch(function(){});
+    }));
+  }));
+});
+self.addEventListener('activate',function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.map(function(k){
+      if(k!==CACHE){return caches.delete(k);}
+    }));
+  }).then(function(){return self.clients.claim();}));
+});
+self.addEventListener('fetch',function(e){
+  if(e.request.method!=='GET'){return;}
+  e.respondWith(
+    caches.match(e.request).then(function(hit){
+      if(hit){return hit;}
+      return fetch(e.request).then(function(res){
+        if(res&&res.status===200&&res.type==='basic'){
+          var copy=res.clone();
+          caches.open(CACHE).then(function(c){c.put(e.request,copy);});
+        }
+        return res;
+      }).catch(function(){
+        return caches.match('/index.html');
+      });
+    })
+  );
+});
